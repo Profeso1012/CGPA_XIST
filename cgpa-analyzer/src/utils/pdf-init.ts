5b2b94d0d1b2f5dc0259e083e25f1d25e2176252
Profeso1012 <140280311+Profeso1012@users.noreply.github.com>
2025-04-01 09:48:23 +0100

    Delete cgpa-analyzer directory

--- cgpa-analyzer/src/utils/pdf-init.ts
+++
@@ -1,8 +0,0 @@
-import * as pdfjsLib from "pdfjs-dist"
-
-// Initialize PDF.js
-export function initPdfJs() {
-  // Set the worker source
-  pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.min.js`
-}
-
